281d038fcd5c362a7418059d3cb2686e99a50343
kelleenkoh <kkjx88@gmail.com>
2023-02-24 15:20:38 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/spacerchild.jpg');
 
 export default function App() {
-  const displayText = useState(false);
-
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}/>
+
+      </View>
+      {/* <Text style ={{color:'#fff'}}>HELLO WORLD!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',/* WANT TO CHANGE TH COLOUR TO FULL BLACK LATER #000000 */
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  imageContainer:{
+    flex:1,
+    paddingTop:30,
+  },
+  image:{
+    width:425,
+    height:440,
+    borderRadius:18,
+  },
+    
+  },
+);
