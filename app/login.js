463fee255afd619e63776f8808b5683535cf6edd
kelleenkoh <kkjx88@gmail.com>
2023-02-28 14:46:32 +0800

    added dropdown picker but the dropdown does not open

--- app/login.js
+++ app/login.js
@@ -1,25 +1,49 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 import StartButton from "../func/StartButton";
 import { StatusBar } from "expo-status-bar";
+import DropDownPicker from "react-native-dropdown-picker";
+import Constants from "expo-constants";
+import useState from 'react-hook-use-state';
+
+
 
 
 export default function Page() {
+
   return (
     <View style ={styles.container}>
+      <Text style = {styles.paragraph}>Smart Spacer </Text>
+      <Text style = {styles.paragraph2}>
+        A one-stop application that helps you to track your doses and asthma condition!
+      </Text>
+      <DropDownPicker
+        items = {[
+          {label:'Alex', value:'item 1', selected: true},
+          {label:'Benard', value:'item 2'},
+          {label:'New User', value:'item 3'}
+        ]}
+        defaultIndex={0}
+        // defaultNull
+        // placeholder= "Select a user"
+        containerStyle = {{height:40}}
+        onChangeItem = {item => console.log(item.label, item.value)}
+      />
+
 
       <View style= {styles.footerContainer}>
 
         <StartButton theme ="primary" label="Login " linkref="/main" />
 
-      
+
       </View>
       <StatusBar style ="auto"/>
     </View>
     
-    
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -27,7 +51,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor:'#8fbc8f',
     justifyContent: 'center',
-    padding: 24,
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
   },
   main: {
     flex: 1,
@@ -44,10 +69,23 @@ const styles = StyleSheet.create({
     color: "#38434D",
   },
   footerContainer: {
-    flex:1/3,
+    flex:2/3,
     alignItems: 'center',
-    
   },
+  paragraph:{
+    margin: 24,
+    fontSize: 24,
+    fontWeight:'bold',
+    textAlign: 'center',
+    color:"#ffffff"
+  },
+  paragraph2:{
+    margin: 24,
+    fontSize: 15,
+    textAlign: 'center',
+    color:"#ffffff"
+  },
+
 });
 
 
