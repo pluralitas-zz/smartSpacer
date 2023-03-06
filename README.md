c75881918045eaae6a5d99df3d817479ab76d838
Cheuk-Yan AU <aucheukyan@gmail.com>
2023-03-06 11:38:35 +0800

    delete picker

--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 
 Use [`expo-router`](https://expo.github.io/router) to build native navigation using files in the `app/` directory.
 
-## 🚀 How to use
+## How to use expo EAS build on Android/iOS
+Ensure that you have java runtime 17, follow guide [here](https://docs.expo.dev/development/create-development-builds/)
 
-```sh
-npx create-react-native-app -t with-router
-```
+or for android only:
+`npx expo start:android`
 
-## 📝 Notes
-
-- [Expo Router: Docs](https://expo.github.io/router)
-- [Expo Router: Repo](https://github.com/expo/router)
-- [Request for Comments](https://github.com/expo/router/discussions/1)
--- app/login.js
+++ app/login.js
@@ -3,33 +3,30 @@ import React,{useState} from "react";
 import { StyleSheet, View, Text } from "react-native";
 import StartButton from "../func/StartButton";
 import { StatusBar } from "expo-status-bar";
-import Constants from "expo-constants";
-import {Picker} from "@react-native-picker/picker";
+import { isArrayLiteralExpression } from "typescript";
+// import Constants from "expo-constants";
 
 export default function Page() {
-  const [pickerValue,setPickerValue] =useState("null")
-  console.log(pickerValue);
+  const [listValue,setListValue] =useState("");
+
+  const username = [
+    {userid: '1', username: "Alex"},
+    {userid: '2', username: "Bernard"},
+  ];
+
   return (
     <View style ={styles.backgroundContainer}>
       <Text style = {styles.title}>Smart Spacer </Text>
       <Text style = {styles.subtitle}> Tracking your medication and condition in a reliable and no-hassle way.</Text>
     
       <View styles = {styles.paragraph}>
-      {/* <Text style = {styles.pickerLabel}>Select User: </Text> */}
-        <Picker
-          style = {styles.picker}
-          selectedValue={pickerValue}
-          onValueChange={(itemValue) =>setPickerValue(itemValue)}
-        >
-          <Picker.Item label ="Select a user" value="null"/>
-          <Picker.Item label ="Alex" value="Alex"/>
-          <Picker.Item label ="Benard" value="Benard"/>
-          <Picker.Item label ="Create New User!" value="Create New User!"/>
-        </Picker>
+
+        <SelectList data={username} setSelected={setListValue} />
+
         </View>
       
       <View style= {styles.loginContainer}>
-        <StartButton theme ="primary" label="Login " linkref="/main" value={pickerValue}/>
+        <StartButton theme ="primary" label="Login " linkref="/main" value={listValue}/>
 
       </View>
       <StatusBar style ="auto"/>
