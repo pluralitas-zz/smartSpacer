bb95b6684f376981c95281de5a4927a0da973ef3
Cheuk-Yan AU <aucheukyan@gmail.com>
2023-04-19 12:29:03 +0800

    BLE working in statistics.js
    Moved BLE to test in statistics page. Working.

--- app/statistics.js
+++ app/statistics.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import React, { useState } from 'react';
 import useBLE from "../useBLE";
